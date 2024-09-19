ed18ed9b2253d2486e2ddfde17ddd7ce7d968b07
K A I S E R <153696501+KAISSOR@users.noreply.github.com>
2024-09-20 01:21:53 +0300

    creating fill java script

--- JavaScript.js
+++ JavaScript.js
@@ -2,19 +2,22 @@
 function openWhatsApp() {
     const name = document.getElementById('name').value;
     const order = document.getElementById('order').value;
+    const adress = document.getElementById('adress').value;
     const phoneNumber = "+201147182463"; 
-    const message = `أهلاً، اسمي ${name} وأود طلب ${order}`;
+    const message = `أهلاً، اسمي ${name} وأود طلب ${order} علي عنوان ${adress}`;
 
     const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
-
     window.open(whatsappUrl, '_blank');
 }
 
 function openTelegram() {
-const username = "+201147182463"; // استبدل بالاسم المستخدم للبوت أو القناة
-const message = encodeURIComponent("رسالتك هنا"); // استبدل بالرسالة التي تريد إرسالها
+const name = document.getElementById('name').value;
+const order = document.getElementById('order').value;
+const adress = document.getElementById('adress').value;
+const username = "+201147182463";
+const message = `أهلاً، اسمي ${name} وأود طلب ${order} علي عنوان ${adress}`;
 
-const telegramUrl = `https://t.me/${username}?start=${message}`;
+const telegramUrl = `https://t.me/${username}?start=${encodeURIComponent(message)}`;
 window.open(telegramUrl, '_blank');
 }
 
